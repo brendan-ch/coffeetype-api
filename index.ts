796c5282c712e32db4529ed19d3c6925e61b9016
Brendan C <smallpigeon1@gmail.com>
2022-05-27 03:35:31 +0000

    add testData endpoint

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import createRoom from './src/api/post/createRoom';
 import exit from './src/api/post/exit';
 import join from './src/api/post/join';
 import start from './src/api/post/start';
+import testData from './src/api/post/testData';
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,6 +18,7 @@ app.post('/api/post/createRoom', createRoom);
 app.post('/api/post/exit', exit);
 app.post('/api/post/join', join);
 app.post('/api/post/start', start);
+app.post('/api/post/testData', testData);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
