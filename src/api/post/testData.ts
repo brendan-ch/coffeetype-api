53bf6ab0fae6bd446d031aab19c49805b4840ec4
Brendan C <smallpigeon1@gmail.com>
2022-05-27 03:34:13 +0000

    add testData endpoint

--- src/api/post/testData.ts
+++ src/api/post/testData.ts
@@ -1,9 +1,49 @@
 // Update the words the player has typed
 
 import { Request, Response } from "express";
+import Logger from "../../helpers/Logger";
+import Player from "../../helpers/Player";
 
+const FILE_PATH = 'api/post/testData.ts';
+
+/**
+ * Save typing data for the user.
+ * @param req
+ * @param res 
+ * @returns 
+ */
 async function testData(req: Request, res: Response) {
-  
+  Logger.print('Endpoint called', FILE_PATH);
+  // Get player ID and room key
+  const roomKey = req.body.roomKey;
+  const playerId = req.body.playerId;
+  const typed = req.body.typed;
+
+  if (typeof roomKey !== 'string' || typeof playerId !== 'string' || typeof typed !== 'string') {
+    Logger.print('Invalid parameters provided', FILE_PATH, true);
+
+    return res.status(400).json({
+      success: false,
+      error: 'Invalid parameters provided.',
+    });
+  }
+
+  // Update the player data
+  const player = Player.findPlayer(playerId);
+  if (!player) {
+    Logger.print('Player not found', FILE_PATH, true);
+
+    return res.status(404).json({
+      success: false,
+      error: 'Player not found.',
+    });
+  }
+
+  player.setTyped(typed);
+
+  res.status(200).json({
+    success: true,
+  });
 }
 
 export default testData;
